75f6f03305ad5d6bd388e82ec9cff7351c448e3b
Cavadadev <148714323+Cavadadev@users.noreply.github.com>
2024-01-03 20:14:02 +0530

    Add files via upload

---
+++ CAVADA.sol
@@ -0,0 +1,88 @@
+pragma solidity ^0.5.18;
+
+import "./ITRC20.sol";
+import "./TRC20.sol";
+
+/**
+ * @dev Optional functions from the TRC20 standard.
+ */
+contract CAVADA is ITRC20,  TRC20 {
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+    address  private _owner;
+    /**
+     * @dev Sets the values for `name`, `symbol`, and `decimals`. All three of
+     * these values are immutable: they can only be set once during
+     * construction.
+     */
+    constructor (string memory name, string memory symbol, uint8 decimals) public {
+        _name = name;
+        _symbol = symbol;
+        _decimals = decimals;
+        _owner = msg.sender;
+    }
+
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    /**
+     * @dev Returns the symbol of the token, usually a shorter version of the
+     * name.
+     */
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5,05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei.
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {ITRC20-balanceOf} and {ITRC20-transfer}.
+     */
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Sender is not owner");
+        _;
+    }
+    /**
+     * @dev Function to mint tokens
+     * @param to The address that will receive the minted tokens.
+     * @param value The amount of tokens to mint.
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function mint(address to, uint256 value) public onlyOwner returns (bool) {
+        _mint(to, value);
+        return true;
+    }
+
+    /**
+     * @dev Burns a specific amount of tokens.
+     * @param value The amount of token to be burned.
+     */
+    function burn(uint256 value) public {
+        _burn(msg.sender, value);
+    }
+
+    /**
+     * @dev Burns a specific amount of tokens from the target address and decrements allowance
+     * @param from address The address which you want to send tokens from
+     * @param value uint256 The amount of token to be burned
+     */
+    function burnFrom(address from, uint256 value) public {
+        _burnFrom(from, value);
+    }
+}
